4fab18b25fc0444f7caca83bc305e1ed8aea9e9d
miquelalcon <miquel.1996@gmail.com>
2018-12-21 22:54:51 +0100

    rm comments from validation

--- src/validation.py
+++ src/validation.py
@@ -7,7 +7,6 @@ if __name__ == '__main__':
     train = pd.read_csv('../data/final_marathon_train.csv')
     test = pd.read_csv('../data/final_marathon_test.csv')
 
-    #elevation_data_path = pd.read_csv('../data/final_elevation_changes.csv')
     runner_obj = runner.Runner(train)
     X = test.drop(runner.Runner.to_predict_names, axis=1)
     y = test[runner.Runner.to_predict_names]
@@ -17,7 +16,4 @@ if __name__ == '__main__':
         pred = runner_obj.run_marathon(X.iloc[i].values)
         pred_res += pred
         res += sum(y.iloc[i])
-        # print(abs(pred-sum(y.iloc[i])))
         print(pred, sum(y.iloc[i]), abs(pred-sum(y.iloc[i])))
-    # print('Total Test instances',len(X))
-    # print('Total difference',abs(res-pred_res))
